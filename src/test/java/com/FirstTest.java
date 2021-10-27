53dfe69e60d58810b1c721681f05b7da325ca197
ChirazG <79950267+ChirazG@users.noreply.github.com>
2021-10-27 15:48:03 +0100

    first commit

---
+++ src/test/java/com/FirstTest.java
@@ -0,0 +1,19 @@
+package com;
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+
+}
